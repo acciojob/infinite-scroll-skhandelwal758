18a3b10a7737a982d2fda4fc6a313da7d58a4f02
skhandelwal758 <65276353+skhandelwal758@users.noreply.github.com>
2023-02-22 22:13:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
-//your code here!
+//your code here
+var listEle = document.querySelector('#infi-list');
+var next = 1;
+var load = function(){
+	for(var i = 0; i < 10; i++){
+		var item = document.createElement('li');
+		item.ineerText = 'Item' + next++;
+		listEle.appendChild(item);
+	}
+}
 
+listEle.addEventListener('scroll', function(){
+	if(listEle.scrollTop + listEle.clientHeight >= listEle.scrollHeight){
+		load();
+	}
+});
+
+load();
